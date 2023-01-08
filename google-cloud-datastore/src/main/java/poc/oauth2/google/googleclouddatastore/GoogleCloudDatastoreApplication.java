d7d2f8596a256423dfafb77e75dc1e7daa716db1
Alberto Coppini <skioppetto@gmail.com>
2023-01-08 13:02:54 +0100

    add new spring initializer project for datastore

---
+++ google-cloud-datastore/src/main/java/poc/oauth2/google/googleclouddatastore/GoogleCloudDatastoreApplication.java
@@ -0,0 +1,13 @@
+package poc.oauth2.google.googleclouddatastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudDatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudDatastoreApplication.class, args);
+	}
+
+}
---
+++ google-cloud-datastore/src/test/java/poc/oauth2/google/googleclouddatastore/GoogleCloudDatastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.oauth2.google.googleclouddatastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloudDatastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
